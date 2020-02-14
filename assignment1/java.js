0682f7eb9de75f9978660653abafa63078e3dd40
BC_rat <56146457+jwthompson1982@users.noreply.github.com>
2020-02-13 23:17:58 -0600

    Created assignment 1 page
    Made assignment 1 page

--- assignment1/java.js
+++ assignment1/java.js
@@ -7,5 +7,5 @@ function openNav() {
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginTop = "1";
+  document.getElementById("main").style.marginTop = "55";
 } 
